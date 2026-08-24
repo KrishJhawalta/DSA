f0f6391f22a5ce35a6b4df37a7db23dece69308d
Krish Jhawalta <krishjhawalta@gmail.com>
2026-08-24 21:14:11 +0530

    Sum of array

---
+++ array/SumofArray.java
@@ -0,0 +1,17 @@
+public class SumofArray{
+  public static void main(String[]args){
+
+    int sum = 0;
+
+    // Array declaration
+    
+    int [] arr = {1,2,3,4,5};
+    for(int i=0; i<arr.length; i++){
+      sum = sum+arr[i];
+
+    }
+    System.out.println("Sum of array: "+sum);
+    
+
+  }
+}
