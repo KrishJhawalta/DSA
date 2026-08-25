6a4c5d43e65f480eb94f84c755a0f5862abe821a
Krish Jhawalta <krishjhawalta@gmail.com>
2026-08-25 21:28:50 +0530

    minimum element

---
+++ array/MinNum.java
@@ -0,0 +1,16 @@
+public class MinNum{
+  public static void main(String[]args){
+
+
+    // Minimum element
+    int num = arr[0];
+
+    int [] arr = {10,20,30,40};
+    for(int i=0; i<arr.length; i++){
+      if(arr[i]<num){
+        num = arr[i];
+      }
+    }
+    System.System.out.println(num);
+  }
+}
