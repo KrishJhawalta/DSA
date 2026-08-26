2bd78a762518446d072cfc6f50bb29a65f19b1cf
Krish Jhawalta <krishjhawalta@gmail.com>
2026-08-26 22:05:06 +0530

    Counting Even Odd Numbers

---
+++ array/EvenOddCount.java
@@ -0,0 +1,18 @@
+public class evencount{
+  public static void main(String[]args){
+
+    int even = 0;
+    int odd = 0;
+    
+    int [] arr = {1,2,3,4,5,6,7,8};
+    for(int i =0; i<arr.length; i++){
+      if(arr[i]%2==0){
+        even++;
+      }else{
+        odd++
+      }
+    }
+    System.out.println("Even: "+even);
+    System.out.println("Odd: "+odd);
+  }
+}
