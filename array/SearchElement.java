9c3d2817d2b92f018272192ee4173593d96f75f5
Krish Jhawalta <krishjhawalta@gmail.com>
2026-08-27 22:54:41 +0530

    updated

--- array/SearchElement.java
+++ array/SearchElement.java
@@ -5,6 +5,8 @@ public class SearchElement{
     int target = 5;
     int [] arr = {1,2,3,4,5};
 
+    // loop
+    //
     for(int i=0; i<arr.length; i++){
       if(target == arr[i]){
         System.out.println(arr[i]);
