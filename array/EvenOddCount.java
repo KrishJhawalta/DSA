aa54c17270858537e2c8fe35d1f48a1fbddd3483
Krish Jhawalta <krishjhawalta@gmail.com>
2026-08-27 22:50:08 +0530

    Updated Code

--- array/EvenOddCount.java
+++ array/EvenOddCount.java
@@ -1,4 +1,4 @@
-public class evencount{
+public class EvenOddCount{
   public static void main(String[]args){
 
     int even = 0;
@@ -7,9 +7,9 @@ public class evencount{
     int [] arr = {1,2,3,4,5,6,7,8};
     for(int i =0; i<arr.length; i++){
       if(arr[i]%2==0){
-        even++;
+        even++;           // counts  even number
       }else{
-        odd++
+        odd++          // counts odd number
       }
     }
     System.out.println("Even: "+even);
