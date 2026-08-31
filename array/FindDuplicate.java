0e7c4fff3c47a41c6b6e68bc6123a83cf57f17f7
Krish Jhawalta <krishjhawalta@gmail.com>
2026-08-31 13:52:55 +0530

    Finding Duplicate Number

--- array/FindDuplicate.java
+++ array/FindDuplicate.java
@@ -1,11 +1,18 @@
 public class FindDuplicate{
   public static void main(String[]args){
 
+    int repeat = 0;
+
     int [] arr = {1,2,3,4,4,5};
     for(int i=0; i<arr.length; i++){
-      if()
-// To be done later
-    }
+      for(int j=i+1; j<arr.length; j++){
+
+      if(arr[i]==arr[j]){
+        repeat = arr[i];
+        break;
+      }}}
+
+  System.out.println("Repeat: "+repeat);
     
   }
 }
