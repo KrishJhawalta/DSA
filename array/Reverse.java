b67310a759f414ef6bb521ed7e170ac65654b0d8
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-01 21:54:02 +0530

    Updated traversal

--- array/Reverse.java
+++ array/Reverse.java
@@ -3,6 +3,7 @@ public class Reverse {
         
         int [] arr = {1,2,3,4,5};
 
+        // reverse printing array ( reverse traversal of array)
         for(int i= arr.length-1; i>0; i--){
             System.out.println(arr[i]);
         }
