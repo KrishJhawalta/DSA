9d5773f5213c432ae9a3f165661a9acab4cbe825
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-02 21:12:47 +0530

    Printing array updated

--- array/PrintArray.java
+++ array/PrintArray.java
@@ -1,6 +1,8 @@
 public class PrintArray{
-  public static void main(String[]args){
 
+  public static void main(String[]args){
+    
+ // Printing array 
     int [] arr = {10,20,30,40,50};
     for(int i = 0; i<arr.length; i++){
     System.out.print(arr[i]);}
