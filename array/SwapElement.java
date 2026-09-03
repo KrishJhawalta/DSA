2fe965d970978a2d10f1e7721eb516db4bbbae35
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-03 11:28:47 +0530

    Advance swapping

--- array/SwapElement.java
+++ array/SwapElement.java
@@ -2,13 +2,32 @@ public class SwapElement {
     public static void main(String[]args){
 
         int [] arr = {1, 2, 3, 4, 5};
-        int temp = arr[0];
-        arr[0] = arr[4];
-        arr[4] = temp;
+        // int temp = arr[0];
+        // arr[0] = arr[4];
+        // arr[4] = temp;
 
-        for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
+        // for(int i = 0; i < arr.length; i++){
+        //     System.out.print(arr[i] + " ");
+        // }
+
+
+        // printing array swapping element 2 to 4 and 1 to 5
+
+        int left =0;
+        int right = arr.length - 1;
+        while(left<right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;     
+            right--;
+            
+    
+
         }
+        for(int i=0; i < arr.length; i++){
+        System.out.print(arr[i]);}
+
     }
     
 }
