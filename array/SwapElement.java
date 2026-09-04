4d198bb96d68f9e1cd8919298b05f7c657af7e77
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-04 21:38:24 +0530

    Array temporary saved

--- array/SwapElement.java
+++ array/SwapElement.java
@@ -1,7 +1,7 @@
 public class SwapElement {
     public static void main(String[]args){
 
-        int [] arr = {1, 2, 3, 4, 5};
+        int [] arr = {10, 20, 30, 40, 50};
         // int temp = arr[0];
         // arr[0] = arr[4];
         // arr[4] = temp;
@@ -13,20 +13,24 @@ public class SwapElement {
 
         // printing array swapping element 2 to 4 and 1 to 5
 
-        int left =0;
-        int right = arr.length - 1;
-        while(left<right){
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;     
-            right--;
+        // int left =0;
+        // int right = arr.length - 1;
+        // while(left<right){
+        //     int temp = arr[left];
+        //     arr[left] = arr[right];
+        //     arr[right] = temp;
+        //     left++;     
+        //     right--;
             
     
 
-        }
-        for(int i=0; i < arr.length; i++){
-        System.out.print(arr[i]);}
+        // }
+        // for(int i=0; i < arr.length; i++){
+        // System.out.print(arr[i]);}
+
+        int temp =arr[0];
+        arr[0] = arr[4];
+        arr[4] = temp;
 
     }
     
