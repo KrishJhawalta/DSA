39b0859b6d99c90473ddf5b71e312fdd5dbc0ea4
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-05 21:14:12 +0530

    searching element advance

--- array/SearchElement.java
+++ array/SearchElement.java
@@ -1,15 +1,18 @@
+import java.util.Scanner;
 public class SearchElement{
   public static void main(String[]args){
 
     // Finding SearchElement 
-    int target = 5;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the element to search");
+    int target = sc.nextInt();
     int [] arr = {1,2,3,4,5};
 
     // loop
-    //
+    
     for(int i=0; i<arr.length; i++){
       if(target == arr[i]){
-        System.out.println(arr[i]);
+        System.out.println(arr[i] + " is found at index " + i);
       }
     }
   }
