8a9dbf100fdafc08bffe06fdb8a879c897e7d740
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-09 21:32:21 +0530

    Modified sort

--- Important Sorting Algorithms/HowToSelectionSort.java
+++ Important Sorting Algorithms/HowToSelectionSort.java
@@ -20,6 +20,10 @@ public class HowToSelectionSort {
             }
         }
     
+        // printing array 
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
        
 
 
