2ff43256eeff08488a2d057a973c67a57e371223
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-10 21:15:45 +0530

    Add selection sort implementation

---
+++ Important Sorting Algorithms/SelectionSortQuestion.java
@@ -0,0 +1,33 @@
+public class SelectionSortQuestion {
+    public static void main(String[]args){
+
+
+
+        int [] arr = {2,4,5,2,55,8,-2,0};
+
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j<arr.length; j++){
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        System.out.println("Sorted array is: ");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
