55c755550683460cd2cd580419603160bf2b3464
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-11 21:23:39 +0530

    Add missing comment for output printing in selection sort

--- Important Sorting Algorithms/SelectionSortQuestion.java
+++ Important Sorting Algorithms/SelectionSortQuestion.java
@@ -17,8 +17,9 @@ public class SelectionSortQuestion {
                     arr[j] = temp;
                 }
             }
-        }
+        } 
 
+        // Printing output
         System.out.println("Sorted array is: ");
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
