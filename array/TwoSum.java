f662f07edff8e39530a21c203f102e017b28a15e
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-12 21:18:30 +0530

    Two Sum Intialisation

---
+++ array/TwoSum.java
@@ -0,0 +1,13 @@
+// This is an practice question
+public class TwoSum{
+  public static void main(String[]argsStrings){
+
+    int [] arr = {2,4,7,6};
+
+    for(int i=0; i<arr.length; i++){
+      for(int j=i+1; j<arr.length; j++){
+        if(arr[i]+arr[j]==8){
+          System.out.println("Pass");
+    }
+      }}
+}
