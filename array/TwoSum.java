7399d395097a971a4772b0000605d587c0f0e81a
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-12 21:22:30 +0530

    Refactor target value handling in TwoSum implementation

--- array/TwoSum.java
+++ array/TwoSum.java
@@ -3,11 +3,16 @@ public class TwoSum{
   public static void main(String[]argsStrings){
 
     int [] arr = {2,4,7,6};
+    int target = 8;
+    
 
     for(int i=0; i<arr.length; i++){
       for(int j=i+1; j<arr.length; j++){
-        if(arr[i]+arr[j]==8){
-          System.out.println("Pass");
+        if(arr[i]+arr[j]==target){
+          System.out.println("Addition of "+arr[i]+" and "+arr[j]+" is equal to target value "+target);
+        }
+        
+      }
     }
-      }}
+  }
 }
