5f9ab85d9268aceba8a5616fbb7c19edaf0e7d21
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-14 21:01:08 +0530

    Bubble sort algorithm

---
+++ Important Sorting Algorithms/HowToBubbleSort.java
@@ -0,0 +1,22 @@
+public class HowToBubbleSort {
+    public static void main(String[]args){
+
+        // 
+        int [] arr = {5,3,8,4,2};
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-i-1; j++){
+
+                // condition
+                if(arr[j]>arr[j+1]){
+                    
+                }
+               
+        }
+
+
+
+
+
+    }
+    
+}
