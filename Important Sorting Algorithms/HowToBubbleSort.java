cfdb9078cd40ce04dda14618442500a14e5a7a53
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-14 21:06:41 +0530

    Add comment to clarify array initialization in bubble sort implementation

--- Important Sorting Algorithms/HowToBubbleSort.java
+++ Important Sorting Algorithms/HowToBubbleSort.java
@@ -1,7 +1,7 @@
 public class HowToBubbleSort {
     public static void main(String[]args){
 
-        // 
+        // array 
         int [] arr = {5,3,8,4,2};
         for(int i=0; i<arr.length-1; i++){
             for(int j=0; j<arr.length-i-1; j++){
