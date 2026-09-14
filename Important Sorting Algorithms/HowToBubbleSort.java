fabf6fc1f2536c0151677ea0a101e5540583b746
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-14 21:01:53 +0530

    Fix formatting and remove unnecessary blank lines in bubble sort implementation

--- Important Sorting Algorithms/HowToBubbleSort.java
+++ Important Sorting Algorithms/HowToBubbleSort.java
@@ -8,7 +8,10 @@ public class HowToBubbleSort {
 
                 // condition
                 if(arr[j]>arr[j+1]){
-                    
+
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
                
         }
