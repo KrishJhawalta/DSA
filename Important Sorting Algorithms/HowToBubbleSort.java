7014960639d6cef2d3ec298d22171b4eba5da6bd
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-15 21:55:19 +0530

    Remove unnecessary blank

--- Important Sorting Algorithms/HowToBubbleSort.java
+++ Important Sorting Algorithms/HowToBubbleSort.java
@@ -8,15 +8,12 @@ public class HowToBubbleSort {
 
                 // condition
                 if(arr[j]>arr[j+1]){
-
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
                
         }
-
-
 
 
 
