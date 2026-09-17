d7eba345dd7b45206ebb8b1a976c774af6cfdf87
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-17 21:53:21 +0530

    bubble question

--- Important Sorting Algorithms/BubbleSortQues.java
+++ Important Sorting Algorithms/BubbleSortQues.java
@@ -1,10 +1,28 @@
 public class BubbleSortQues{
     public static void main(String[]args){
         
-        int [] arr = {4,1,3,9,7};
-        int n = arr.length;
+        // int [] arr = {4,1,3,9,7};
+        // int n = arr.length;
         
-        for(int i=0; i<n-1; i++){
+        // for(int i=0; i<n-1; i++){
+        //     for(int j=0; j<n-i-1; j++){
+        //         if(arr[j]>arr[j+1]){
+        //             int temp = arr[j];
+        //             arr[j] = arr[j+1];
+        //             arr[j+1] = temp;
+        //         }
+        //     }
+        // }
+        // for(int k =0; k<n; k++){
+        // System.out.print(arr[k] + " ");}
+
+
+        // Q2: 
+
+        int [] arr ={6,4,1};
+
+        int n = arr.length;
+        for(int i =0; i<n; i++){
             for(int j=0; j<n-i-1; j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
@@ -13,7 +31,10 @@ public class BubbleSortQues{
                 }
             }
         }
-        for(int k =0; k<n; k++){
-        System.out.print(arr[k] + " ");}
+        for(int k: a){
+            System.out.println("Array is sorted in +  + swaps");
+            System.out.println("First element is: "+ arr[0]);
+            System.out.println("Last element is: "+ arr[n-1]);
+    }
     }
 }
