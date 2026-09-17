6567a7301abd7fb6635af8bc1a57f716af453c2d
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-17 21:55:06 +0530

    Fix bubble sort output and count swaps

--- Important Sorting Algorithms/BubbleSortQues.java
+++ Important Sorting Algorithms/BubbleSortQues.java
@@ -20,21 +20,24 @@ public class BubbleSortQues{
         // Q2: 
 
         int [] arr ={6,4,1};
+        int swaps = 0;
 
         int n = arr.length;
         for(int i =0; i<n; i++){
             for(int j=0; j<n-i-1; j++){
                 if(arr[j]>arr[j+1]){
+
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swaps++;
                 }
             }
         }
-        for(int k: a){
-            System.out.println("Array is sorted in +  + swaps");
+     
+            System.out.println("Array is sorted in " + swaps + " swaps");
             System.out.println("First element is: "+ arr[0]);
             System.out.println("Last element is: "+ arr[n-1]);
-    }
+    
     }
 }
