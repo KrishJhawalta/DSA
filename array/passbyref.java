48c93bd4dfb515294f26f6903d80464f60b0c88c
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-19 22:15:15 +0530

    Add passbyref class to reverse an array in place

---
+++ array/passbyref.java
@@ -0,0 +1,16 @@
+public class passbyref{
+    public static void main(String[]args){
+
+        int [] arr = {1,2,3,4,5};
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    
+}
