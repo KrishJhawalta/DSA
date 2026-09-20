f6cd51ff474785f93f2b47bd49d5e63d37b6d9c9
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-20 22:20:05 +0530

    Comments in pass by ref

--- array/passbyref.java
+++ array/passbyref.java
@@ -1,6 +1,8 @@
 public class passbyref{
     public static void main(String[]args){
+ 
 
+        // array 
         int [] arr = {1,2,3,4,5};
         int i=0;
         int j=arr.length-1;
@@ -8,6 +10,8 @@ public class passbyref{
 
         // loop
         while(i<j){
+
+            // changing value
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
