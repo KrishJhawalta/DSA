cfd8eab157967dee1ccf3878dac831e67d94d3a9
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-23 16:34:27 +0530

    Remove demo.txt and add test.java with initial structure

---
+++ TimeComplexity/test.java
@@ -0,0 +1,13 @@
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        
+        // time complexity
+        for(int i=0; i<10; i++){
+            System.out.println("Hello World");
+        }
+    }
+}
