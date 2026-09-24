aa0d60cc38d25af295a38ae4e07355314ec2f2e1
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-24 21:40:39 +0530

    Pattern

--- LogicBuilding/Pattern1.java
+++ LogicBuilding/Pattern1.java
@@ -15,5 +15,19 @@ public class Pattern1{
                 System.out.print("*");
             }System.out.println();
         }
+
+
+        /*
+        Pattern 2
+        *
+        **
+        ***
+        ****
+         */
+        for(int i=0; i<4; i++){
+            for(int j=0; j<i; j++){
+                System.out.print("*");
+            }System.out.println();
+        }
     }
 }
