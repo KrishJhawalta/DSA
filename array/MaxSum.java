b6c25546e15cefdb649b981b680333598836251e
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-24 21:31:41 +0530

    Logic Building

--- array/MaxSum.java
+++ array/MaxSum.java
@@ -1,10 +1,11 @@
 public class MaxSum{
   public static void main(String[]args){
 
+    int [] arr = {1,2,3,4,5};
     // array value to compare
     int max = arr[0];
 
-    int [] arr = {1,2,3,4,5};
+    
     for(int i=0; i<arr.length; i++){
       if(arr[i]>max){
         max = arr[i];
