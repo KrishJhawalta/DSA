476768b59c0a10c7c08638894318c126a52725fa
Krish Jhawalta <krishjhawalta@gmail.com>
2026-09-25 21:53:37 +0530

    Add structure for Pattern 3

--- LogicBuilding/Pattern1.java
+++ LogicBuilding/Pattern1.java
@@ -27,6 +27,22 @@ public class Pattern1{
             for(int j=0; j<i; j++){
                 System.out.print("*");
             }System.out.println();
+
+
+        }
+
+        /*
+        Pattern 3
+        ****
+        ***
+        **
+        * 
+        */
+
+        for(int i=0; i<4; i++){
+            for(int j=4; j>i; j--){
+                System.out.print("*");
+            }System.out.println();
         }
     }
 }
